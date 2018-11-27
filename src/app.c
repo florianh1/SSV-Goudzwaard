dfbcf2936ce557f7d2ff853a8cb335263dc67e7c
Henri <henrivandemunt@gmail.com>
2018-11-27 13:57:43 +0100

    Added task for receiving control commando's
    - New task for receiving control commands from app using udp port 3333
    - default monitor speed 115200 (platfomio.ini)

--- src/app.c
+++ src/app.c
@@ -14,32 +14,36 @@
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
 
+/* Bling GPIO pin */
 #define BLINK_GPIO 2
 
 /* Wi-Fi SSID, note a maximum character length of 32 */
-#define ESP_WIFI_SSID       "SSV GoudZwaard"
+#define ESP_WIFI_SSID "SSV GoudZwaard"
 
 /* Wi-Fi password, note a minimal length of 8 and a maximum length of 64 characters */
-#define ESP_WIFI_PASS       "12345678"
+#define ESP_WIFI_PASS "12345678"
 
 /* Wi-Fi channel, note a range from 0 to 13 */
-#define ESP_WIFI_CHANNEL    0
+#define ESP_WIFI_CHANNEL 0
 
 /* Amount of Access Point connections, note a maximum amount of 4 */
-#define AP_MAX_CONNECTIONS  4
+#define AP_MAX_CONNECTIONS 4
 
 /* Port number of TCP-server */
-#define TCP_PORT                3000
+#define TCP_PORT 3000
 
 /* Maximum queue length of pending connections */
-#define LISTEN_QUEUE        2
+#define LISTEN_QUEUE 2
 
 /* Size of the recvieve buffer (amount of charcters) */
-#define RECV_BUF_SIZE       64
+#define RECV_BUF_SIZE 64
+
+/* Port number of UDP-server */
+#define UDP_PORT 3333
 
 /* Pre-defined delay times, in seconds */
-#define TASK_DELAY_1     1000
-#define TASK_DELAY_5     5000
+#define TASK_DELAY_1 1000
+#define TASK_DELAY_5 5000
 
 const int CLIENT_CONNECTED_BIT = BIT0;
 const int CLIENT_DISCONNECTED_BIT = BIT1;
@@ -47,7 +51,6 @@ const int AP_STARTED_BIT = BIT2;
 
 static const char *TAG = "Wifi";
 static EventGroupHandle_t wifi_event_group;
-
 
 /**
  * Handle events triggerd by the ESPs RTOS system. Called automatically on event
@@ -59,24 +62,25 @@ static EventGroupHandle_t wifi_event_group;
  */
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
-    switch(event->event_id) {
+    switch (event->event_id)
+    {
     case SYSTEM_EVENT_AP_START:
-		printf("Event: ESP32 is started in AP mode\n");
+        printf("Event: ESP32 is started in AP mode\n");
         xEventGroupSetBits(wifi_event_group, AP_STARTED_BIT);
         break;
-		
-	case SYSTEM_EVENT_AP_STACONNECTED:
-        printf("Event: station connected to AP\n");
-		xEventGroupSetBits(wifi_event_group, CLIENT_CONNECTED_BIT);
-		break;
 
-	case SYSTEM_EVENT_AP_STADISCONNECTED:
+    case SYSTEM_EVENT_AP_STACONNECTED:
+        printf("Event: station connected to AP\n");
+        xEventGroupSetBits(wifi_event_group, CLIENT_CONNECTED_BIT);
+        break;
+
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
         printf("Event: station disconnected from AP\n");
-		xEventGroupSetBits(wifi_event_group, CLIENT_DISCONNECTED_BIT);
-		break;
+        xEventGroupSetBits(wifi_event_group, CLIENT_DISCONNECTED_BIT);
+        break;
     case SYSTEM_EVENT_AP_PROBEREQRECVED:
         printf("Event: probe request recieved\n");
-        break;	
+        break;
     default:
         break;
     }
@@ -91,9 +95,10 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
 void nvs_init()
 {
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
@@ -147,7 +152,8 @@ void wifi_init()
         },
     };
 
-    if (strlen(ESP_WIFI_SSID) == 0) {
+    if (strlen(ESP_WIFI_SSID) == 0)
+    {
         ESP_LOGW(TAG, "Password lenght is 0, changing authmode to OPEN...");
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
@@ -156,7 +162,7 @@ void wifi_init()
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "wifi setup finished. SSID: %s pass: %s",
-            wifi_config.ap.ssid, wifi_config.ap.password);
+             wifi_config.ap.ssid, wifi_config.ap.password);
 }
 
 /**
@@ -185,16 +191,19 @@ void tcp_server(void *pvParam)
 
     xEventGroupWaitBits(wifi_event_group, AP_STARTED_BIT, false, true, portMAX_DELAY);
 
-    while(1) {
+    while (1)
+    {
         s = socket(AF_INET, SOCK_STREAM, 0);
-        if (s < 0) {
+        if (s < 0)
+        {
             ESP_LOGE(TAG, "Failed to allocate socket");
             vTaskDelay(TASK_DELAY_1 / portTICK_PERIOD_MS);
             continue;
         }
 
         ESP_LOGI(TAG, "Allocated socket");
-        if (bind(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0) {
+        if (bind(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0)
+        {
             ESP_LOGE(TAG, "Socket bind failed, errno:%d", errno);
             close(s);
             vTaskDelay(TASK_DELAY_1 / portTICK_PERIOD_MS);
@@ -202,30 +211,35 @@ void tcp_server(void *pvParam)
         }
 
         ESP_LOGI(TAG, "Socket bind done");
-        if (listen(s, LISTEN_QUEUE) != 0) {
+        if (listen(s, LISTEN_QUEUE) != 0)
+        {
             ESP_LOGE(TAG, "Socket listen failed errno:%d", errno);
             close(s);
             vTaskDelay(TASK_DELAY_1 / portTICK_PERIOD_MS);
             continue;
         }
 
-        while (1) {
+        while (1)
+        {
             ESP_LOGI(TAG, "Now accepting socket connections...");
             cs = accept(s, (struct sockaddr *)&remote_addr, &socklen);
             ESP_LOGI(TAG, "New connection request, request data:");
             fcntl(cs, F_SETFL, O_NONBLOCK);
             bzero(recv_buf, sizeof(recv_buf));
-            do {
+            do
+            {
                 r = recv(cs, recv_buf, sizeof(recv_buf) - 1, 0);
-                for (int i = 0; i < r; i++) {
+                for (int i = 0; i < r; i++)
+                {
                     putchar(recv_buf[i]);
                 }
             } while (r > 0);
             printf("\n");
 
             ESP_LOGI(TAG, "Done reading from socket. Last read return:%d, errno:%d", r, errno);
-            
-            if (write(cs, recv_buf, sizeof(recv_buf)) < 0) { // TODO: dont send whole recv_buf, only filled part
+
+            if (write(cs, recv_buf, sizeof(recv_buf)) < 0)
+            { // TODO: dont send whole recv_buf, only filled part
                 ESP_LOGE(TAG, "Send failed");
                 close(s);
                 vTaskDelay(TASK_DELAY_1 / portTICK_PERIOD_MS);
@@ -236,8 +250,8 @@ void tcp_server(void *pvParam)
             close(cs);
         }
 
-    ESP_LOGI(TAG, "Server will be opend in 5 seconds");
-    vTaskDelay(TASK_DELAY_5 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Server will be opend in 5 seconds");
+        vTaskDelay(TASK_DELAY_5 / portTICK_PERIOD_MS);
     }
 
     ESP_LOGI(TAG, "tcp_client task closed");
@@ -262,12 +276,13 @@ void printStationList()
     ESP_ERROR_CHECK(esp_wifi_ap_get_sta_list(&wifi_sta_list));
     ESP_ERROR_CHECK(tcpip_adapter_get_sta_list(&wifi_sta_list, &adapter_sta_list));
 
-    for(int i = 0; i < adapter_sta_list.num; i++) {
+    for (int i = 0; i < adapter_sta_list.num; i++)
+    {
         tcpip_adapter_sta_info_t station = adapter_sta_list.sta[i];
         printf("%d - mac: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x - IP: %s\n", i + 1,
-                station.mac[0], station.mac[1], station.mac[2],
-				station.mac[3], station.mac[4], station.mac[5],
-                ip4addr_ntoa(&(station.ip)));
+               station.mac[0], station.mac[1], station.mac[2],
+               station.mac[3], station.mac[4], station.mac[5],
+               ip4addr_ntoa(&(station.ip)));
     }
 
     printf("\n");
@@ -282,12 +297,16 @@ void printStationList()
 void print_sta_info(void *pvParam)
 {
     ESP_LOGI(TAG, "print_sta_info task started \n");
-    while (1) {
+    while (1)
+    {
         EventBits_t staBits = xEventGroupWaitBits(wifi_event_group, CLIENT_CONNECTED_BIT | CLIENT_DISCONNECTED_BIT, pdTRUE, pdFALSE, portMAX_DELAY);
-        
-        if ((staBits & CLIENT_CONNECTED_BIT) != 0) {
+
+        if ((staBits & CLIENT_CONNECTED_BIT) != 0)
+        {
             ESP_LOGI(TAG, "New station connected");
-        } else {
+        }
+        else
+        {
             ESP_LOGI(TAG, "A station disconnected");
         }
 
@@ -295,6 +314,11 @@ void print_sta_info(void *pvParam)
     }
 }
 
+/**
+ * Blink task
+ *
+ * @return void
+ */
 void blink_task(void *pvParameter)
 {
     /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
@@ -306,7 +330,8 @@ void blink_task(void *pvParameter)
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
+    while (1)
+    {
         /* Blink off (output low) */
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -316,6 +341,111 @@ void blink_task(void *pvParameter)
     }
 }
 
+/**
+ * receive control task
+ *
+ * @return void
+ */
+void receive_control_task(void *pvParameter)
+{
+    ESP_LOGI(TAG, "receive_control_task task started");
+
+    char rx_buffer[128];
+    char addr_str[128];
+    int addr_family;
+    int ip_protocol;
+
+    int payload;
+
+    uint8_t joystick_y;
+    uint8_t joystick_x;
+    uint8_t scrollbar;
+
+    while (1)
+    {
+
+        struct sockaddr_in destAddr;
+        destAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        destAddr.sin_family = AF_INET;
+        destAddr.sin_port = htons(UDP_PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+        inet_ntoa_r(destAddr.sin_addr, addr_str, sizeof(addr_str) - 1);
+
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0)
+        {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created");
+
+        int err = bind(sock, (struct sockaddr *)&destAddr, sizeof(destAddr));
+        if (err < 0)
+        {
+            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        }
+        ESP_LOGI(TAG, "Socket binded");
+
+        while (1)
+        {
+
+            ESP_LOGI(TAG, "receive_control_task waiting for data");
+            struct sockaddr_in6 sourceAddr; // Large enough for both IPv4 or IPv6
+            socklen_t socklen = sizeof(sourceAddr);
+            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&sourceAddr, &socklen);
+
+            // Error occured during receiving
+            if (len < 0)
+            {
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                break;
+            }
+            // Data received
+            else
+            {
+                // Get the sender's ip address as string
+                if (sourceAddr.sin6_family == PF_INET)
+                {
+                    inet_ntoa_r(((struct sockaddr_in *)&sourceAddr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
+                }
+                else if (sourceAddr.sin6_family == PF_INET6)
+                {
+                    inet6_ntoa_r(sourceAddr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+                }
+
+                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string...
+
+                // extract rx_buffer to payload 
+                sscanf(rx_buffer, "%d", &payload);
+
+                // extract payload to values
+                scrollbar = payload & 0x3F;
+                joystick_y = (payload >> 6) & 0x7;
+                joystick_x = (payload >> 9) & 0x7;
+
+                ESP_LOGI(TAG, "scrollbar: %d", scrollbar);
+                ESP_LOGI(TAG, "joystick_y: %d", joystick_y);
+                ESP_LOGI(TAG, "joystick_x: %d", joystick_x);
+
+                int err = sendto(sock, rx_buffer, len, 0, (struct sockaddr *)&sourceAddr, sizeof(sourceAddr));
+                if (err < 0)
+                {
+                    ESP_LOGE(TAG, "Error occured during sending: errno %d", errno);
+                    break;
+                }
+            }
+        }
+
+        if (sock != -1)
+        {
+            ESP_LOGE(TAG, "Shutting down socket and restarting...");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
 
 /**
  * Main loop
@@ -324,14 +454,20 @@ void blink_task(void *pvParameter)
  */
 void app_main()
 {
+    // Start blink task for testing
+    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+
+    // Initialization of Non-Volitile Storage
     nvs_init();
 
+    // Init WiFI
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
     wifi_event_group = xEventGroupCreate();
     start_dhcp_server();
     wifi_init();
 
-    xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
-    xTaskCreate(&tcp_server, "tcp_server", 4096, NULL, 5, NULL);
-    xTaskCreate(&print_sta_info, "print_sta_info", 4096, NULL, 5, NULL);
+    xTaskCreate(&receive_control_task, "receive_control_task", 4096, NULL, 5, NULL);
+
+    //xTaskCreate(&tcp_server, "tcp_server", 4096, NULL, 5, NULL);
+    //xTaskCreate(&print_sta_info, "print_sta_info", 4096, NULL, 5, NULL);
 }
