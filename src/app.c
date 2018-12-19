aafb31f9c6f1d0eb523a01b83fcddec13277a596
Florian-Hagens <florian-hagens@hotmail.com>
2018-12-19 13:28:25 +0100

    changed the priorities

--- src/app.c
+++ src/app.c
@@ -41,7 +41,7 @@ TaskHandle_t receive_control_task_handler = NULL;
 TaskHandle_t control_syringe_task_handler = NULL;
 TaskHandle_t motor_task_handler = NULL;
 TaskHandle_t light_task_handler = NULL;
-TaskHandle_t control_task_handler = NULL;
+TaskHandle_t task_control_handler = NULL;
 
 extern EventGroupHandle_t wifi_event_group;
 extern const int CLIENT_CONNECTED_BIT;
@@ -135,10 +135,10 @@ void app_main()
     ESP_LOGI(APP_MAIN_TAG, "Device connected, starting tasks!");
 
     // start the tasks
-    xTaskCreate(&camera_task, "camera_task", 4096 * 2, NULL, 5, &camera_task_handler);
-    xTaskCreate(&battery_percentage_transmit_task, "battery_percentage_transmit_task", 2048, NULL, 5, &battery_percentage_transmit_task_handler);
-    xTaskCreate(&receive_control_task, "receive_control_task", 2048 * 2, NULL, 5, &receive_control_task_handler);
-    xTaskCreate(&control_syringe_task, "control_syringe_task", 4096, NULL, 5, &control_syringe_task_handler);
+    xTaskCreate(&camera_task, "camera_task", 4096 * 2, NULL, 6, &camera_task_handler);
+    xTaskCreate(&battery_percentage_transmit_task, "battery_percentage_transmit_task", 2048, NULL, 7, &battery_percentage_transmit_task_handler);
+    xTaskCreate(&receive_control_task, "receive_control_task", 2048 * 2, NULL, 3, &receive_control_task_handler);
+    xTaskCreate(&control_syringe_task, "control_syringe_task", 4096, NULL, 4, &control_syringe_task_handler);
     xTaskCreate(&motor_task, "motor_task", 2048, NULL, 5, &motor_task_handler);
-    xTaskCreate(&task_control, "task_control", 2048, NULL, 5, &task_control_handler);
+    xTaskCreate(&task_control, "task_control", 2048, NULL, 2, &task_control_handler);
 }
